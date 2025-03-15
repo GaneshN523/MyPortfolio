4437fd5d414c71a714c5cc1cf1451cd0bfa85759
GaneshN523 <ganeshnalawade523@gmail.com>
2025-03-16 00:05:56 +0530

    MyPortfolio

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/FirmManagement">
+    <BrowserRouter basename="/MyPortfolio">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
