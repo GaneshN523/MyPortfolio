7a9a66ca6faf2cbc62e97ea4cc55e66b1513f03b
GaneshN523 <ganeshnalawade523@gmail.com>
2025-03-17 09:58:59 +0530

    Text Line

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,46 +1,49 @@
 import React from 'react';
 import styles from './Projects.module.css';
-import intelligentsensebot from '../assets/Projects/intelligentsensebot.jfif'
-import FirmManagement from '../assets/Projects/FirmManagement.webp'
-import ProgrammingVisualizer from '../assets/Projects/ProgrammingVisualizer.webp'
-import SQLVisualizer from '../assets/Projects/SQLVisualizer.webp'
+import intelligentsensebot from '../assets/Projects/intelligentsensebot.jfif';
+import FirmManagement from '../assets/Projects/FirmManagement.webp';
+import ProgrammingVisualizer from '../assets/Projects/ProgrammingVisualizer.webp';
+import SQLVisualizer from '../assets/Projects/SQLVisualizer.webp';
 
 // Sample projects data
-// Sample projects data
 const projectsData = [
-    {
-      id: 1,
-      name: 'IntelligentSense Bot',
-      image: intelligentsensebot,
-      visitUrl: 'https://ganeshshubh.github.io/CollegeAssist/',
-      codeUrl: 'https://huggingface.co/spaces/GaneshN523/Institute/tree/main',
-    },
-    {
-      id: 2,
-      name: 'FirmManagement',
-      image: FirmManagement,
-      visitUrl: 'https://GaneshN523.github.io/FirmManagement/',
-      codeUrl: 'https://github.com/GaneshN523/FirmManagement/',
-    },
-    {
-      id: 3,
-      name: 'Programming Visualizer',
-      image: ProgrammingVisualizer,
-      visitUrl: 'https://GaneshN523.github.io/prog-vi/',
-      codeUrl: 'https://github.com/GaneshN523/prog-vi/',
-    },
-    {
-      id: 4,
-      name: 'SQL Visualizer',
-      image: SQLVisualizer,
-      visitUrl: 'https://GaneshN523.github.io/sql-vi/',
-      codeUrl: 'https://github.com/GaneshN523/sql-vi/',
-    },
-    // Add more projects as needed...
-  ];
-  
-  const Projects = () => {
-    return (
+  {
+    id: 1,
+    name: 'IntelligentSense Bot',
+    image: intelligentsensebot,
+    visitUrl: 'https://ganeshshubh.github.io/CollegeAssist/',
+    codeUrl: 'https://huggingface.co/spaces/GaneshN523/Institute/tree/main',
+  },
+  {
+    id: 2,
+    name: 'FirmManagement',
+    image: FirmManagement,
+    visitUrl: 'https://GaneshN523.github.io/FirmManagement/',
+    codeUrl: 'https://github.com/GaneshN523/FirmManagement/',
+  },
+  {
+    id: 3,
+    name: 'Programming Visualizer',
+    image: ProgrammingVisualizer,
+    visitUrl: 'https://GaneshN523.github.io/prog-vi/',
+    codeUrl: 'https://github.com/GaneshN523/prog-vi/',
+  },
+  {
+    id: 4,
+    name: 'SQL Visualizer',
+    image: SQLVisualizer,
+    visitUrl: 'https://GaneshN523.github.io/sql-vi/',
+    codeUrl: 'https://github.com/GaneshN523/sql-vi/',
+  },
+  // Add more projects as needed...
+];
+
+const Projects = () => {
+  return (
+    <div>
+      {/* Hacker-style heading */}
+      <div className={styles.hackerText}>Visit the projects on the desktop for better visuals</div>
+      
       <div className={styles.projectsGrid}>
         {projectsData.map((project) => (
           <div key={project.id} className={styles.projectCard}>
@@ -75,7 +78,8 @@ const projectsData = [
           </div>
         ))}
       </div>
-    );
-  };
-  
-  export default Projects;
+    </div>
+  );
+};
+
+export default Projects;
